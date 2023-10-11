06169e2930b8272ec7100ed76cea5d3ee9eaec29
han <rudgksdl94@gmail.com>
2023-10-11 21:42:36 +0900

    shop 상세조회 서비스, 컨트롤러, 리스폰dto

--- src/main/java/com/beauty/api/model/shop/service/ShopService.java
+++ src/main/java/com/beauty/api/model/shop/service/ShopService.java
@@ -1,5 +1,7 @@
 package com.beauty.api.model.shop.service;
 
+import com.beauty.api.model.shop.dto.ShopResponse;
+import com.beauty.api.model.shop.persist.entity.ShopEntity;
 import com.beauty.api.model.shop.persist.repository.ShopRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +11,13 @@ import org.springframework.stereotype.Service;
 public class ShopService {
 
   private final ShopRepository shopRepository;
+
+  public ShopResponse getShop(Long id) {
+    ShopEntity shopEntity = this.shopRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 샵이 존재하지 않습니다."));
+
+    return ShopResponse.fromEntity(shopEntity);
+  }
+
 
 }
---
+++ src/test/java/com/beauty/api/model/shop/controller/ShopControllerTest.java
@@ -0,0 +1,21 @@
+package com.beauty.api.model.shop.controller;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopControllerTest {
+
+  @BeforeAll
+  void setUp() {
+  }
+  
+  @Test
+  void getShopList() {
+  }
+
+  @Test
+  void getShop() {
+  }
+}
