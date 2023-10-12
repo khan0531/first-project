5f390c309f14c89e864dcabf42be119e0d4e8f2a
han <rudgksdl94@gmail.com>
2023-10-12 14:07:13 +0900

    MemberEntity, AdminMemberEntity에서 UserDetails 분리

--- src/main/java/com/beauty/api/model/user/persist/entity/MemberEntity.java
+++ src/main/java/com/beauty/api/model/user/persist/entity/MemberEntity.java
@@ -1,12 +1,9 @@
 package com.beauty.api.model.user.persist.entity;
 
 import com.beauty.api.model.user.dto.constants.Authority;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -22,16 +19,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity(name = "member")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class MemberEntity implements UserDetails {
+public class MemberEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -61,38 +55,4 @@ public class MemberEntity implements UserDetails {
   private LocalDateTime createdAt;
 
   private LocalDateTime updatedAt;
-
-  @Override
-  @JsonIgnore
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return this.roles.stream()
-        .map(Enum::name)
-        .map(SimpleGrantedAuthority::new)
-        .collect(Collectors.toList());
-  }
-
-  @Override
-  public String getUsername() {
-    return email;
-  }
-
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
-
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
-
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
-
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
 }
--- src/main/java/com/beauty/api/security/TokenProvider.java
+++ src/main/java/com/beauty/api/security/TokenProvider.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -48,6 +49,7 @@ public class TokenProvider {
         .compact();
   }
 
+  @Transactional
   public Authentication getAuthentication(String jwt) {
     UserDetails userDetails = this.memberService.loadUserByUsername(this.getUsername(jwt));
     return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
