c9497e45ab1f0b356497139c3319c716f69401fa
han <rudgksdl94@gmail.com>
2023-10-12 11:26:53 +0900

    리뷰 상세 조회 api

--- src/main/java/com/beauty/api/model/review/service/ReviewService.java
+++ src/main/java/com/beauty/api/model/review/service/ReviewService.java
@@ -1,5 +1,7 @@
 package com.beauty.api.model.review.service;
 
+import com.beauty.api.model.review.dto.ReviewResponse;
+import com.beauty.api.model.review.persist.entity.ReviewEntity;
 import com.beauty.api.model.review.persist.repository.ReviewRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,11 @@ import org.springframework.stereotype.Service;
 public class ReviewService {
 
   private final ReviewRepository reviewRepository;
+
+  public ReviewResponse getReview(Long id) {
+    ReviewEntity reviewEntity = this.reviewRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 리뷰가 존재하지 않습니다."));
+
+    return ReviewResponse.fromEntity(reviewEntity);
+  }
 }
