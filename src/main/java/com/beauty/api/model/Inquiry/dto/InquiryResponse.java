c2f49d6b8796b0ec5125039f614bca9a4766b5de
han <rudgksdl94@gmail.com>
2023-10-13 11:50:44 +0900

    문의 작성 api

---
+++ src/main/java/com/beauty/api/model/Inquiry/dto/InquiryResponse.java
@@ -0,0 +1,41 @@
+package com.beauty.api.model.Inquiry.dto;
+
+import com.beauty.api.model.Inquiry.persist.entity.InquiryEntity;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class InquiryResponse {
+
+  private String title;
+
+  private String content;
+
+  private String image;
+
+  private Long memberId;
+
+  private Long shopId;
+
+  private LocalDateTime createdAt;
+
+  private LocalDateTime updatedAt;
+
+  public static InquiryResponse fromEntity(InquiryEntity inquiryEntity) {
+    return InquiryResponse.builder()
+        .title(inquiryEntity.getTitle())
+        .content(inquiryEntity.getContent())
+        .image(inquiryEntity.getImage())
+        .memberId(inquiryEntity.getMember().getId())
+        .shopId(inquiryEntity.getShop().getId())
+        .createdAt(inquiryEntity.getCreatedAt())
+        .updatedAt(inquiryEntity.getUpdatedAt())
+        .build();
+  }
+}
