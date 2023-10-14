b4a7722098e782dcdb2e5703943d49727b285ade
han <rudgksdl94@gmail.com>
2023-10-14 16:43:06 +0900

    Member updateDTO 수정

--- src/main/java/com/beauty/api/model/user/dto/MemberUpdateRequest.java
+++ src/main/java/com/beauty/api/model/user/dto/MemberUpdateRequest.java
@@ -1,7 +1,6 @@
 package com.beauty.api.model.user.dto;
 
 import java.time.LocalDate;
-import javax.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +13,6 @@ import lombok.NoArgsConstructor;
 public class MemberUpdateRequest {
 
   private Long id;
-
-  @Email(message = "이메일 형식이 아닙니다.")
-  private String email;
 
   private String phone;
 
