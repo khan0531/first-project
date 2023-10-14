7ebb7771041dec279350735d45ba5130f5a7c238
han <rudgksdl94@gmail.com>
2023-10-14 15:59:31 +0900

    멤버 validation 위치 변경

--- src/main/java/com/beauty/api/model/user/dto/MemberSignUpRequest.java
+++ src/main/java/com/beauty/api/model/user/dto/MemberSignUpRequest.java
@@ -5,6 +5,8 @@ import com.beauty.api.model.user.persist.entity.MemberEntity;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,13 +19,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MemberSignUpRequest {
 
+  @NotBlank(message = "이름을 입력해주세요.")
   private String name;
 
+  @Email(message = "이메일 형식이 아닙니다.")
   private String email;
 
   @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
   private String password;
 
+  @NotBlank(message = "전화번호를 입력해주세요.")
   private String phone;
 
   private LocalDate birth;
